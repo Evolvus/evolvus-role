6c29be91bd791ecdf26d75a9463c1b314bae6529
KmKavya <kavyavshaiva@gmail.com>
2018-06-14 16:59:45 +0530

    changed DbUrl

--- test/db/role.js
+++ test/db/role.js
@@ -5,7 +5,7 @@
  const expect = chai.expect;
  const role = require("../../db/role");
 
- var MONGO_DB_URL = process.env.MONGO_DB_URL || "mongodb://10.10.69.204/Platform_Dev";
+ var MONGO_DB_URL = process.env.MONGO_DB_URL || "mongodb://10.10.69.204/TestPlatform_Dev";
 
  chai.use(chaiAsPromised);
 
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
   const chai = require("chai");
   const mongoose = require("mongoose");
 
-  var MONGO_DB_URL = process.env.MONGO_DB_URL || "mongodb://10.10.69.204/TestroleCollection";
+  var MONGO_DB_URL = process.env.MONGO_DB_URL || "mongodb://10.10.69.204/TestPlatform_Dev";
   /*
    ** chaiAsPromised is needed to test promises
    ** it adds the "eventually" property
