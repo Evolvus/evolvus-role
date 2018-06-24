ade5a1830cdc4ffc3d5f198b2752263dbac985c7
Shriman Nijagun <EVOLVUS\shrimank@ELPBLR-025.evolvus.com>
2018-06-24 05:43:02 +0530

    exported roleSchema.

--- db/roleSchema.js
+++ db/roleSchema.js
@@ -1,90 +1,90 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const {
-  menu
+    menu
 } = require("evolvus-menu");
 
 var Menu = mongoose.model("menuCollection", menu.menuDBschema);
 
 var roleSchema = new mongoose.Schema({
-  // Add all attributes below tenantId
-  tenantId: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 64
-  },
-  applicationCode: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 20
-  },
-  roleName: {
-    type: String,
-    required: true,
-    unique: true,
-    minLength: 1,
-    maxLength: 20,
-    validate: {
-      validator: function(v) {
-        return /^[A-Za-z ]*$/.test(v);
-      },
-      message: "{PATH} can contain only alphabets and spaces"
-    }
-  },
-  menuGroup: {
-    type: Array,
-    minItems: 1,
-    items: {
-      ref: 'Menu'
+    // Add all attributes below tenantId
+    tenantId: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 64
     },
-    required: true
-  },
-  description: {
-    type: String,
-    minLength: 0,
-    maxLength: 255,
-    validate: {
-      validator: function(v) {
-        return /^[A-Za-z ]*$/.test(v);
-      },
-      message: "{PATH} can contain only alphabets and spaces"
+    applicationCode: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 20
+    },
+    roleName: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 1,
+        maxLength: 20,
+        validate: {
+            validator: function(v) {
+                return /^[A-Za-z ]*$/.test(v);
+            },
+            message: "{PATH} can contain only alphabets and spaces"
+        }
+    },
+    menuGroup: {
+        type: Array,
+        minItems: 1,
+        items: {
+            ref: 'Menu'
+        },
+        required: true
+    },
+    description: {
+        type: String,
+        minLength: 0,
+        maxLength: 255,
+        validate: {
+            validator: function(v) {
+                return /^[A-Za-z ]*$/.test(v);
+            },
+            message: "{PATH} can contain only alphabets and spaces"
+        }
+    },
+    createdBy: {
+        type: String,
+        required: true
+    },
+    updatedBy: {
+        type: String
+    },
+    createdDate: {
+        type: Date,
+        required: true
+    },
+    lastUpdatedDate: {
+        type: Date
+    },
+    enableFlag: {
+        type: Number,
+        default: 1
+    },
+    deletedFlag: {
+        type: Number,
+        default: 0
+    },
+    activationStatus: {
+        type: String,
+        required: true
+    },
+    processingStatus: {
+        type: String,
+        required: true
+    },
+    associatedUsers: {
+        type: Number,
+        required: true
     }
-  },
-  createdBy: {
-    type: String,
-    required: true
-  },
-  updatedBy: {
-    type: String
-  },
-  createdDate: {
-    type: Date,
-    required: true
-  },
-  lastUpdatedDate: {
-    type: Date
-  },
-  enableFlag: {
-    type: Number,
-    default: 1
-  },
-  deletedFlag: {
-    type: Number,
-    default: 0
-  },
-  activationStatus: {
-    type: String,
-    required: true
-  },
-  processingStatus: {
-    type: String,
-    required: true
-  },
-  associatedUsers: {
-    type: Number,
-    required: true
-  }
 });
 module.exports = roleSchema;
